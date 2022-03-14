ab850e3dc5fd4be5d4648ff4b5031ca6f7397b72
Umesh <umes4ever@gmail.com>
2022-03-14 22:08:21 +0545

    Init commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import { useTranslation } from "react-i18next";
 
 function App() {
+  const { t, i18n } = useTranslation();
+
+  const changeLanguage = (language) => {
+    i18n.changeLanguage(language);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="App-header">
+        <div>
+          <p>{t("title")}</p>
+        </div>
+        <div>
+          <label htmlFor="languages">{t("select-label")}: </label>
+          <select
+            name="languages"
+            id="languages"
+            onChange={(e) => changeLanguage(e.target.value)}
+          >
+            <option value="en">English</option>
+            <option value="np">Nepali</option>
+            <option value="jp">Japanese</option>
+          </select>
+        </div>
+        <div>
+          <p>{t("description.sample-text")}</p>
+        </div>
+      </div>
     </div>
   );
 }
